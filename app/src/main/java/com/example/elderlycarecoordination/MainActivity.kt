d0544ef0218c6e422c15373a111edc6e98275566
ASUS1 <pandiarchana.palaniappan@my.jcu.edu.au>
2025-02-16 13:02:16 +0800

    Fixed MainActivity.kt

--- app/src/main/java/com/example/elderlycarecoordination/MainActivity.kt
+++ app/src/main/java/com/example/elderlycarecoordination/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -21,7 +18,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.*
 
